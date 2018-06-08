8c1db380de6825ff5d5884912a71d9cecde457a0
EXT.CVELAKATURICHOWD <EXT.CVELAKATURICHOWD@INCHEDTDM1800.INCHESSC.COM>
2018-06-08 12:16:22 +0530

    gg

---
+++ src/stepDefinition/Test_Steps.java
@@ -0,0 +1,56 @@
+package stepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test_Steps {
+	
+	public static WebDriver driver;
+	@Given("^User is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ext.cvelakaturichowd\\Downloads\\chromedriver_win32 (1)\\chromeDriver.exe");	
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.store.demoqa.com");
+		}
+ 
+	@When("^User Navigate to LogIn Page$")
+	public void user_Navigate_to_LogIn_Page() throws Throwable {
+		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+		}
+ 
+	@And("^User enters \"(.*)\" and \"(.*)\"$")
+	public void user_enters_UserName_and_Password(String username, String password) throws Throwable {
+		driver.findElement(By.id("log")).sendKeys("username"); 	 
+	    driver.findElement(By.id("pwd")).sendKeys("password");
+	    driver.findElement(By.id("login")).click();
+		}
+ 
+	@Then("^Message displayed Login Successfully$")
+	public void message_displayed_Login_Successfully() throws Throwable {
+		System.out.println("Login Successfully");
+	}
+	
+ 
+	@When("^User LogOut from the Application$")
+	public void user_LogOut_from_the_Application() throws Throwable {
+		driver.findElement (By.xpath("//*[@id=\"meta\"]//li[2]/a")).click();
+	}
+	
+	@Then("^Message displayed LogOut Successfully$")
+	public void message_displayed_LogOut_Successfully() {
+		System.out.println("LogOut Successfully");
+		driver.close();
+	}
+
+
+}
